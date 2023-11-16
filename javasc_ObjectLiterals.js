b80fae8cb5263834563a695f7b43cd9684285dd6
Ashhish-h <kushwaha.ashish2611@gmail.com>
2023-11-16 15:33:12 +0530

    new file added javasc_ObjectLiterals

---
+++ javasc_ObjectLiterals.js
@@ -0,0 +1,13 @@
+// ########################## OBJECT LITERALS ################################
+
+// Object literl are the data structure that stores values in pair Form(key, value).
+
+let student = {
+    name: "Ashish",
+    age: 18,
+};
+
+// here student contains values in key and value form
+// name is the key have value Ashish
+
+
