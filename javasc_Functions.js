816f8695af4e6ad7dbc2154450d7f440e6736d69
Ashhish-h <kushwaha.ashish2611@gmail.com>
2023-11-17 12:27:55 +0530

    Added javasc_Functions

---
+++ javasc_Functions.js
@@ -0,0 +1,12 @@
+// ############################ Functions ###############################
+
+// ## Functions are used to increase readability of the code and it also reduces the lenght of the code
+
+// #######   Syntax 
+function functionName (){
+    //Body of the code
+    console.log("Example");
+}
+
+// ## We have to call the function by its fucntion name followed by parenthesis
+functionName();
